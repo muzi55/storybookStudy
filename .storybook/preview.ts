02345ac269e3c1a4d1e6943319e4aa44d8bd9ec0
muzi55 <muzi5@kakao.com>
2024-03-10 20:33:24 +0900

    스토리북 css 연동

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,7 @@
 import type { Preview } from "@storybook/react";
+import "@fontsource/noto-sans-kr/400.css"; // Specify weight
+import "@fontsource/noto-sans-kr/700.css"; // Specify weight
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
